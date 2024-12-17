6efda83f2786b0fb969059fecda15890f4f34add
nguyenvietbinh <126882221+nguyenvietbinh@users.noreply.github.com>
2024-12-17 08:49:54 +0000

    rebuild wellcome page

--- nuxt-project/components/homePageComponents/header.vue
+++ nuxt-project/components/homePageComponents/header.vue
@@ -1,9 +1,9 @@
 <template>
-    <header class="header w-full h-[12.5vh] fixed bg-[#ffffff] opacity-[0.5] transition-all duration-300">
+    <header class="header w-full h-[12.5vh] absolute bg-[#ffffff] opacity-[0.5] transition-all duration-300">
         <NuxtLink to="/">
             <img src="/img/logo.png" alt="" class="logo h-[50px] cursor-pointer w-auto absolute left-[5vw] md:left-[2.5vw] top-[50%] translate-y-[-50%]">
         </NuxtLink>
-        <div @click="moveToSource" class="source hidden text-[20px] absolute bg-[#ffffff] cursor-pointer text-[#001f1b] hover:bg-[#828282] md:inline-block left-[140px] rounded-[22px] p-[4px] px-4 top-[50%] translate-y-[-50%] hover:translate-y-[-53%] transition duration-300 opacity-100">Source</div>
+        <div @click="moveToSource" class="source hidden text-[20px] absolute cursor-pointer text-[#001f1b] hover:bg-[#828282] md:inline-block left-[10vw] rounded-[22px] p-[4px] px-4 top-[50%] translate-y-[-50%] hover:translate-y-[-53%] transition duration-300 opacity-100">Source</div>
         <NuxtLink to="/signin" class="absolute right-[7.5vw] md:right-[5vw] text-[#001f1b] font-semibold hover:underline top-[50%] translate-y-[-50%] transition duration-300">Sign In</NuxtLink>
     </header>
 </template>
--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,13 +1,17 @@
 <template>
     <HomePageComponentsHeader/>
-    <h1 class="absolute hidden md:block text-[50px] font-semibold text-white top-[50%] translate-y-[-70%] text-center left-[50%] translate-x-[-50%]">FOLLOW YOUR DREAM AND JOIN WITH US NOW</h1>
-    <NuxtLink to="/bettable" class="top-[60%] rounded-[35px] hover:text-primary hover:bg-white font-semibold hover:top-[59%] text-center left-[50%] translate-x-[-50%] absolute inline-block text-[40px] text-white border-white border-[4px] border-solid px-10 transition-all duration-300">PLAY</NuxtLink>
+    <div class="h-[85vh] w-full absolute top-[12.5vh]">
+        <h1 class="absolute hidden md:block text-[40px] font-thin font-serif text-gray-500 select-none top-[40%] translate-y-[-90%] text-center left-[50%] translate-x-[-50%]">PLAY POKER WITH STRONGEST AI IN THE WORLD NOW.</h1>
+        <h1 class="absolute block md:hidden text-[40px] font-thin font-serif text-gray-600 select-none top-[30%] text-center left-[50%] translate-x-[-50%]">Wellcome.</h1>
+        <NuxtLink to="/bettable" class="md:top-[45%] select-none top-[40%] rounded-[35px] font-serif hover:text-black hover:bg-white font-thin md:hover:top-[44%] text-center left-[50%] translate-x-[-50%] absolute inline-block text-[40px] text-white border-white border-[2px] border-solid px-10 transition-all duration-300">PLAY</NuxtLink>
+    </div>
+    <HomePageComponentsFooter class=""/>
 </template>
 
 
 <style>
 body {
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     background-image: linear-gradient(rgba(0, 0, 0, 0.75),rgba(0, 0, 0, 0.75)),url(/img/homePageBackground.png);
     background-size: cover;
