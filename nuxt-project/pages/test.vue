79e158a23d9832513112aada3de47f7f1df0d718
nguyenvietbinh <nguyenvietbinh006@gamil.com>
2025-02-24 09:23:45 +0700

    add cards

---
+++ nuxt-project/pages/test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div :class="`${style.card} bottom-[50%] left-[45%]`">
+        <img src="/img/cards/clubs_k.png" alt="">
+    </div>
+</template>
+
+
+<script>
+import { style } from '~/store/data/taildwindStyle';
+export default {
+    setup() {
+        return {
+            style
+        }
+    }
+}
+</script>
